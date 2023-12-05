9ad76e25fd37c98831cccdb471abc60ab4a4b437
Patricia E. Miguel <contacto@patriciaemiguel.com>
2023-12-05 15:05:30 -0300

    feat: create models and migration

--- game_stats/models.py
+++ game_stats/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255)
+    profile_image = models.URLField()
+
+    def __str__(self):
+        return f'{self.nickname} - {self.profile_image}'
+
+class Stat(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.creation_date} - {self.score}'
