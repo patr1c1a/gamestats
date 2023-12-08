7bf2188c5dfbebf35299abad332e9cb7e8a8e601
Patricia E. Miguel <contacto@patriciaemiguel.com>
2023-12-07 23:52:18 -0300

    feat: added validation to models

--- game_stats/models.py
+++ game_stats/models.py
@@ -1,4 +1,7 @@
+from django.utils import timezone
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
 class Player(models.Model):
@@ -18,7 +21,7 @@ class Stat(models.Model):
     """
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     creation_date = models.DateTimeField(auto_now_add=True)
-    score = models.IntegerField()
+    score = models.PositiveIntegerField()
 
     def __str__(self):
         return f"(ID: {self.pk}). PLAYER: {self.player.nickname}. SCORE: {self.score}. CREATED: {self.creation_date}."
@@ -26,13 +29,30 @@ class Stat(models.Model):
 
 class Game(models.Model):
     """
-    Details on a game involving 0 to 10 players.
+    Details about a game involving 0 to 10 players.
     """
-    players = models.ManyToManyField(Player, related_name='games')
+    players = models.ManyToManyField(Player, related_name='players')
     start_timestamp = models.DateTimeField(auto_now_add=True)
     finish_timestamp = models.DateTimeField(null=True, blank=True)
     winner = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        # If start_timestamp is not set, set it to the current date and time
+        if not self.start_timestamp:
+            self.start_timestamp = timezone.now()
+
+        super().save(*args, **kwargs)
+
+        # Check if the winner is in the list of players after the instance is saved
+        if self.winner and self.winner not in self.players.all():
+            raise ValidationError({
+                'winner': ValidationError('Winner must be in the players list.', code='invalid'),
+            })
+
+        # Check that finish date is later than start date.
+        if self.finish_timestamp and self.start_timestamp and self.finish_timestamp <= self.start_timestamp:
+            raise ValidationError("Finish date must be later than start date.")
+
     def __str__(self):
-        return f"(ID: {self.pk}). PLAYERS: {', '.join([str(player.nickname) for player in self.players.all()])}. " \
-               f"START: {self.start_timestamp}. FINISH: {self.finish_timestamp}. WINNER: {self.winner.nickname}"
+        winner = self.winner.nickname if self.winner else 'No winner yet'
+        return f"(ID: {self.pk}). START: {self.start_timestamp}. FINISH: {self.finish_timestamp}. WINNER: {winner}"
