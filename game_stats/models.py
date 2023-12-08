04989c2672b9f73bfcd896bc332e967a1cfe1e1a
Patricia E. Miguel <contacto@patriciaemiguel.com>
2023-12-08 11:52:07 -0300

    fix: make profile image optional in Player model

--- game_stats/models.py
+++ game_stats/models.py
@@ -8,7 +8,7 @@ class Player(models.Model):
     A single player.
     """
     nickname = models.CharField(max_length=255)
-    profile_image = models.URLField()
+    profile_image = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return f"(ID: {self.pk}). NICKNAME: {self.nickname}. AVATAR: {self.profile_image}"
