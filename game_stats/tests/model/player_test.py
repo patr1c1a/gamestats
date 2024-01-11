15d3ef6200fd4d2ed0f0b68bf06b4185c9e3fcbe
Patricia E. Miguel <contacto@patriciaemiguel.com>
2024-01-11 11:39:41 -0300

    test: modify model.player test cases to consider the new user field.

--- game_stats/tests/model/player_test.py
+++ game_stats/tests/model/player_test.py
@@ -1,31 +1,35 @@
 from django.test import TestCase
 from game_stats.models import Player
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 
 class PlayerModelTest(TestCase):
+
+	def setUp(self):
+		self.user = User.objects.create(username="test_user", password="test_password")
+		self.player = Player.objects.create(
+			user=self.user,
+			nickname="test_player",
+			profile_image="https://example.com/image.jpg"
+		)
+
 	def test_player_creation(self):
 		"""
 	    Tests that a Player can be created with all its fields.
 		"""
-		player = Player.objects.create(
-			nickname="test_player",
-			profile_image="https://example.com/image.jpg"
-		)
-		self.assertEqual(player.nickname, "test_player")
-		self.assertEqual(player.profile_image, "https://example.com/image.jpg")
+
+		self.assertEqual(self.player.nickname, "test_player")
+		self.assertEqual(self.player.profile_image, "https://example.com/image.jpg")
+		self.assertEqual(self.player.user.username, "test_user")
 
 	def test_delete_player(self):
 		"""
 		Tests that Player can be deleted.
 		"""
-		player = Player.objects.create(
-			nickname="test_player",
-			profile_image="https://example.com/image.jpg"
-		)
-		player.delete()
+		self.player.delete()
 		with self.assertRaises(Player.DoesNotExist):
-			Player.objects.get(id=player.id)
+			Player.objects.get(id=self.player.id)
 
 	def test_player_no_profile_image(self):
 		"""
@@ -33,6 +37,7 @@ class PlayerModelTest(TestCase):
 		"""
 		player = Player.objects.create(
 			nickname="test_player",
+			user=User.objects.create(username="test_user2", password="test_password")
 		)
 		self.assertEqual(player.nickname, "test_player")
 
@@ -41,20 +46,33 @@ class PlayerModelTest(TestCase):
 		Tests that creating a Player without a nickname raises a ValidationError.
 		"""
 		player = Player(
-			profile_image="https://example.com/image.jpg"
+			profile_image="https://example.com/image.jpg",
+			user=User.objects.create(username="test_user3", password="test_password")
 		)
 		with self.assertRaises(ValidationError) as context:
 			player.full_clean()
 		self.assertIn('nickname', context.exception.error_dict)
 
+
+	def test_player_no_user(self):
+		"""
+		Tests that creating a Player without a user raises a ValidationError.
+		"""
+		player = Player(
+			nickname="test_player",
+		)
+		with self.assertRaises(ValidationError) as context:
+			player.full_clean()
+		self.assertIn('user', context.exception.error_dict)
+
 	def test_player_invalid_nickname(self):
 		"""
 	    Tests that a Player nickname can only contain letters, numbers and underscores.
 		"""
-		player = Player(nickname="test player")
+		self.player.nickname = "test player"
 
 		with self.assertRaises(ValidationError) as context:
-			player.save()
+			self.player.save()
 
 		self.assertIn(
 			"Nickname can only contain letters, numbers and underscores.",
