b0e18c1d8390385cbefec5b0c7509e6b11a8aaff
Patricia E. Miguel <contacto@patriciaemiguel.com>
2024-01-11 11:42:29 -0300

    test: modify serializer.player test cases to consider the new user field.

--- game_stats/tests/serializer/player_test.py
+++ game_stats/tests/serializer/player_test.py
@@ -1,13 +1,16 @@
 from django.test import TestCase
 from game_stats.serializers import PlayerSerializer
+from django.contrib.auth.models import User
 
 
 class PlayerSerializerTest(TestCase):
+
     def test_valid_nickname(self):
         """
         Tests that a player created with a valid nickname is valid.
         """
-        data = {"nickname": "test_player"}
+        user = User.objects.create(username='test_user1', password='test_password')
+        data = {"user": user.id, "nickname": "test_player"}
         serializer = PlayerSerializer(data=data)
         self.assertTrue(serializer.is_valid())
 
@@ -15,7 +18,8 @@ class PlayerSerializerTest(TestCase):
         """
         Tests that a player created with an invalid nickname raises an error.
         """
-        data = {"nickname": "test player"}
+        user = User.objects.create(username='test_user2', password='test_password')
+        data = {"user": user.id, "nickname": "test player"}
         serializer = PlayerSerializer(data=data)
         self.assertFalse(serializer.is_valid())
         self.assertIn("Nickname can only contain letters, numbers, and underscores.", serializer.errors["nickname"])
