27d2bff960e0cf5508955c48525369c2c21f9dda
Patricia E. Miguel <contacto@patriciaemiguel.com>
2024-01-17 21:34:43 -0300

    refactor: use reverse for urls.

--- game_stats/tests/view/stat_test.py
+++ game_stats/tests/view/stat_test.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
-from game_stats.models import Stat, Player
+from rest_framework_simplejwt.tokens import AccessToken
+from game_stats.models import Stat, Player, User
 from django.utils import timezone
 
 
@@ -10,18 +12,22 @@ class StatViewsTest(TestCase):
 		"""
 		Creates test data.
 		"""
-		self.player1 = Player.objects.create(nickname="stat_view_test_player1")
+		self.user1 = User.objects.create(username="test_user1", password="test_password", is_staff="True")
+		self.player1 = Player.objects.create(user=self.user1, nickname="stat_view_test_player1")
 		self.stat1 = Stat.objects.create(player=self.player1, creation_date=timezone.now(), score=10)
 		self.stat2 = Stat.objects.create(player=self.player1, creation_date=timezone.now(), score=5)
 		self.stat3 = Stat.objects.create(player=self.player1, creation_date=timezone.now(), score=1)
+
+		self.access_token = str(AccessToken.for_user(self.user1))
 		self.client = APIClient()
+		self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
 
 	def test_get_stats(self):
 		"""
-		Tests the /stats/ endpoint by validating that stats added during test setUp are returned, asserting their
+		Tests GET to /stats/ endpoint by validating that stats added during test setUp() are returned, asserting their
 		scores and player nicknames.
 		"""
-		response = self.client.get('/stats/')
+		response = self.client.get(reverse("stat-list-or-create"))
 		returned_stats = [stat for stat in response.data["results"]]
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertEqual(self.stat1.score, returned_stats[0]["score"])
@@ -33,9 +39,9 @@ class StatViewsTest(TestCase):
 
 	def test_get_stat_detail(self):
 		"""
-		Tests the stats/<int:pk>/ endpoint by retrieving a specific stat by its id.
+		Tests GET to stats/<int:pk>/ endpoint by retrieving a specific stat by its id.
 		"""
-		response = self.client.get(f'/stats/{self.stat1.id}/')
+		response = self.client.get(reverse("stat-by-id", args=[self.stat1.id]), format="json")
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertEqual(response.data['player']['id'], self.player1.id)
 		self.assertEqual(response.data['score'], self.stat1.score)
