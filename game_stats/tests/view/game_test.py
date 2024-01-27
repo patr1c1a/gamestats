dd61c596ea2aee2ad975c28061adc9f41631a26d
Patricia E. Miguel <contacto@patriciaemiguel.com>
2024-01-27 11:25:41 -0300

    test: add tests for Game view to verify delete and patch cannot be performed as a non admin user.

--- game_stats/tests/view/game_test.py
+++ game_stats/tests/view/game_test.py
@@ -11,25 +11,28 @@ class GameViewsTest(TestCase):
 		"""
 		Creates test data. Logs in as an admin (is_staff=True) user.
 		"""
-		self.user1 = User.objects.create(username="test_user1", password="test_password", is_staff=True)
-		self.user2 = User.objects.create(username="test_user2", password="test_password")
-		self.player1 = Player.objects.create(user=self.user1, nickname="game_view_test_player1")
-		self.player2 = Player.objects.create(user=self.user2, nickname="game_view_test_player2")
+		self.admin_user = User.objects.create(username="test_user1", password="test_password", is_staff=True)
+		self.non_admin_user = User.objects.create(username="test_user2", password="test_password", is_staff=False)
+		self.player1 = Player.objects.create(user=self.admin_user, nickname="game_view_test_player1")
+		self.player2 = Player.objects.create(user=self.non_admin_user, nickname="game_view_test_player2")
 		self.game1 = Game.objects.create()
 		self.game1.players.set([self.player1, self.player2])
 		self.game1.winner = self.player1
 		self.game2 = Game.objects.create()
 		self.game2.players.set([])
 
-		self.access_token = str(AccessToken.for_user(self.user1))
-		self.client = APIClient()
-		self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+		self.access_token = str(AccessToken.for_user(self.admin_user))
+		self.admin_client = APIClient()
+		self.admin_client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+		self.non_admin_access_token = str(AccessToken.for_user(self.non_admin_user))
+		self.non_admin_client = APIClient()
+		self.non_admin_client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.non_admin_access_token}')
 
 	def test_get_games(self):
 		"""
 		Tests GET to /games/ endpoint by validating that nicknames of all players added during test setUp are returned.
 		"""
-		response = self.client.get(reverse("game-list-or-create"))
+		response = self.admin_client.get(reverse("game-list-or-create"))
 		returned_nicknames = []
 		for game in response.data["results"]:
 			for player in game.get("players"):
@@ -42,7 +45,7 @@ class GameViewsTest(TestCase):
 		"""
 		Tests GET to /games/<int:pk>/ endpoint by retrieving a specific game by its id.
 		"""
-		response = self.client.get(reverse("game-by-id", args=[self.game1.id]), format="json")
+		response = self.admin_client.get(reverse("game-by-id", args=[self.game1.id]), format="json")
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertEqual(response.data["id"], self.game1.id)
 
@@ -50,7 +53,7 @@ class GameViewsTest(TestCase):
 		"""
 		Tests GET to /games/<int:pk> (no trailing slash) endpoint by retrieving a specific game by its id.
 		"""
-		response = self.client.get(f"/games/{self.game1.id}")
+		response = self.admin_client.get(f"/games/{self.game1.id}")
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertEqual(response.data["id"], self.game1.id)
 
@@ -59,28 +62,56 @@ class GameViewsTest(TestCase):
 		Tests POST to /games/ to create a new game.
 		"""
 		data = {"players": [self.player1.id, self.player2.id], "winner": self.player1.id}
-		response = self.client.post(reverse("game-list-or-create"), data=data, format="json")
+		response = self.admin_client.post(reverse("game-list-or-create"), data=data, format="json")
 		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-	def test_update_game(self):
+	def test_update_game_as_admin(self):
 		"""
-		Tests PATCH to /games/<int:pk>/ endpoint by updating a specific game.
+		Tests PATCH to /games/<int:pk>/ endpoint by updating a specific game as an admin user.
 		"""
 		updated_data = {"winner": self.player2.id}
-		response = self.client.patch(reverse("game-by-id", args=[self.game1.id]), data=updated_data, format="json")
+		response = self.admin_client.patch(reverse("game-by-id", args=[self.game1.id]), data=updated_data,
+		                                   format="json")
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 		# Check database:
 		self.game1.refresh_from_db()
 		self.assertEqual(self.game1.winner, self.player2)
 
-	def test_delete_game(self):
+	def test_update_game_as_non_admin(self):
 		"""
-		Tests DELETE to /games/<int:pk>/ endpoint by deleting a specific game.
+        Tests PATCH to /games/<int:pk>/ endpoint is not permitted to a non-admin user.
+        """
+		updated_data = {"winner": self.player1.id}
+		response = self.non_admin_client.patch(reverse("game-by-id", args=[self.game2.id]), data=updated_data,
+		                                       format="json")
+		self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+		# Check that the game wasn't updated:
+		self.game2.refresh_from_db()
+		self.assertIsNone(self.game2.winner)
+
+	def test_delete_game_as_admin(self):
 		"""
-		response = self.client.delete(reverse("game-by-id", args=[self.game1.id]))
+		Tests DELETE to /games/<int:pk>/ endpoint by deleting a specific game as an admin user.
+		"""
+		response = self.admin_client.delete(reverse("game-by-id", args=[self.game1.id]))
 		self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 		# Check database:
 		with self.assertRaises(Game.DoesNotExist):
 			Game.objects.get(id=self.game1.id)
+
+	def test_delete_game_as_non_admin(self):
+		"""
+		Tests DELETE to /games/<int:pk>/ endpoint is not permitted to a non-admin user.
+		"""
+		response = self.non_admin_client.delete(reverse("game-by-id", args=[self.game2.id]))
+		self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+		# Check database:
+		try:
+			game2 = Game.objects.get(id=self.game2.id)
+		except Game.DoesNotExist:
+			game2 = None
+		self.assertIsNotNone(game2, "Game should still exist after deletion attempt.")
